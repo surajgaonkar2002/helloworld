8b6467a9da1a527306a4f043e3598598ad35524b
admin <admin@Bcalab4-m07>
2022-03-03 10:58:33 +0530

    s

--- casetool2/src/casetool2/student.java
+++ casetool2/src/casetool2/student.java
@@ -5,6 +5,7 @@ public class student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("student");
+//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
 	}
 
 }
